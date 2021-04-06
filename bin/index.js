d6b0c9fe6157871bd0305ff2749a2e1bb805957d
李世豪 <515634453@qq.com>
2021-04-06 20:53:10 +0800

    feat: complete v1

--- bin/index.js
+++ bin/index.js
@@ -1,13 +1,26 @@
 #!/usr/bin/env node
 
 const program = require('commander');
-const prompts = require('../lib/create');
+const { createApp, createTemplate } = require('../lib/create');
 
+/**
+ * init project
+ */
 program
-    .command('create <name>')
-    .description('create a new react project')
+    .command('init <name>')
+    .description('Create a new react app')
     .action(name => {
-        prompts(name);
+        createApp(name);
+    });
+
+/**
+ * create a view or component
+ */
+program
+    .command('create')
+    .description('Create a view or component template in your project')
+    .action(() => {
+        createTemplate();
     });
 
 program.parse();
---
+++ lib/templates/views/index.jsx
@@ -0,0 +1,7 @@
+import styles from './widget.module.scss';
+
+const PageContainer = () => {
+    return <div></div>;
+};
+
+export default PageContainer;
